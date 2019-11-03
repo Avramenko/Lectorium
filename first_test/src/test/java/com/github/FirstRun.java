2f513aa81a3301ee7d35510356e6d36a9c0aca90
Avramenko <avramenko.r.v@gmail.com>
2019-11-03 22:04:47 +0200

    Property file

--- first_test/src/test/java/com/github/FirstRun.java
+++ first_test/src/test/java/com/github/FirstRun.java
@@ -2,23 +2,24 @@ package com.github;
 
 import org.junit.Assert;
 import org.junit.Test;
-import pages.BlogPage;
+import util.PropertyLoader;
 
 public class FirstRun extends WebDriverSetting {
     /*String loginField = ".//input[@id = 'login_field']";
     String passwordField = ".//input[@id = 'password']";
-    String signInButton = ".//input[@value = 'Sign in']";*/
+    String signInButton = ".//input[@value = 'Sign in']";
+    String blogForHtml = "https://github.blog";
+    String blogLinkButton = ".//li[@class = 'mb-1']//a[text() = 'Blog']";
+    String expectedPostPageTitle = "GitHub";*/
     String userNameValue = "testQA8120";
     String passwordValue = "TestQwe123rty";
-    String blogForHtml = "https://github.blog";
     String expectedLogoBlog = "The GitHub Blog";
-    String blogLinkButton = ".//li[@class = 'mb-1']//a[text() = 'Blog']";
-    String expectedPostPageTitle = "GitHub";
+
 
     @Test
     public void FirstRun() {
         driver.manage().window().maximize();
-        driver.get("https://github.com/login");
+        driver.get(PropertyLoader.loadProperty("environment.login"));
         loginPage.enterFirstName(userNameValue);
         loginPage.enterPassword(passwordValue);
         loginPage.ClickLoginButton();
@@ -28,7 +29,6 @@ public class FirstRun extends WebDriverSetting {
         postLoginPage.ClickBlogLink();
 
         Assert.assertEquals("https://github.blog/", driver.getCurrentUrl());
-        BlogPage blogPage = new BlogPage(driver, wait);
         Assert.assertEquals(blogPage.checkLogo(),expectedLogoBlog);
 
 
